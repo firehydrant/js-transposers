f6b75de0d04bd424d3b9ced09b89755a52ad5cfa
Vinny Thanh <vthanh@firehydrant.com>
2024-11-27 14:38:19 -0700

    Add a template transposer js file to repo

---
+++ src/template.js
@@ -0,0 +1,17 @@
+////////////////////
+// COPY FROM HERE //
+/*
+ * Transpose a payload into a Signal
+ */
+function transpose(input) {
+  const payload = input.data;
+
+  // TODO: Edit your transposer code here. For examples, see other files in the repository
+  let signal = {};
+  return signal;
+}
+// COPY TO HERE //
+//////////////////
+module.exports = {
+  transpose
+}
