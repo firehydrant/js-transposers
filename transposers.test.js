c31b7238ede2a1b7b0c23cdd1ba03f29dea04a18
Vinny Thanh <vthanh@firehydrant.com>
2024-12-03 11:37:22 -0700

    Tests check execution time and output match

--- transposers.test.js
+++ transposers.test.js
@@ -2,56 +2,45 @@ const fs = require('fs');
 const glob = require('glob');
 const path = require('path');
 
-const transposers = [
-  {
-    name: 'aws-sns',
-    cases: ['open', 'confirm']
-  },
-  {
-    name: 'conviva',
-    cases: ['open']
-  },
-  {
-    name: 'fastly',
-    cases: ['open']
-  },
-  {
-    name: 'gcp-stackdriver-single',
-    cases: ['open', 'closed']
-  },
-  {
-    name: 'pd-v2',
-    cases: ['open', 'resolve']
-  }
-].map((transposer) => Object.assign(transposer, { toString: function() { return this.name; }}));
+const transposers = require('./cases.json').map((transposer) => Object.assign(transposer, { toString: function() { return this.name; }}));
 
 describe.each(transposers)('%s', (transposer) => {
-  let name = transposer.name;
-  let cases = transposer.cases;
+  const name = transposer.name;
+  const cases = transposer.cases;
+  let transpose;
+  let input, expected;
+
+  beforeAll(async () => {
+    // Get the transposer file and load it
+    let file = `./src/${name}.js`;
+    transpose = await require(file).transpose;
+
+  });
 
   for (const testCase of cases) {
-    it(`correctly transposes ${testCase} case`, async () => {
-      // Get the transposer file and load it
-      let file = `./src/${name}.js`;
-      let { transpose } = await require(file);
+    let input, expected, output;
+    // Load input and expected output JSONs
+    // Then run input through transposer function and test output
+    let inputFile = `./src/cases/${name}/${testCase}.input.json`;
+    let expectedFile = `./src/cases/${name}/${testCase}.expected.json`;
+    try {
+      let inputJson = JSON.parse(fs.readFileSync(inputFile, 'utf8'));
+      input = {
+        data: inputJson
+      };
+      expected = JSON.parse(fs.readFileSync(expectedFile, 'utf8'));
+    } catch (err) {
+      throw err;
+    }
+    it(`${testCase} - should transpose in under 50 ms`, async () => {
+      const startTime = performance.now();
+      output = transpose(input);
+      const endTime = performance.now();
+      expect(endTime - startTime).toBeLessThan(50);
+    });
 
-      // Load input and expected output JSONs
-      // Then run input through transposer function and test output
-      let inputFile = `./src/cases/${name}/${testCase}.input.json`;
-      let expectedFile = `./src/cases/${name}/${testCase}.expected.json`;
-      try {
-        let inputJson = JSON.parse(fs.readFileSync(inputFile, 'utf8'));
-        const input = {
-          data: inputJson
-        };
-        const expected = JSON.parse(fs.readFileSync(expectedFile, 'utf8'));
-
-        let output = transpose(input);
-        expect(output).toEqual(expected);
-
-      } catch (err) {
-        throw err;
-      }
+    it(`${testCase} - output should match expected`, async () => {
+      expect(output).toEqual(expected);
     });
   }
 });
